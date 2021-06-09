0c7b6769d23cca067bead2d0e26dd5c36d093b78
rofifaghna <85244975+rofifaghna@users.noreply.github.com>
2021-06-09 19:05:16 +0700

    Add files via upload

---
+++ sistem/config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+File: config.php
+Fungsi: Koneksi ke Database
+Auth: ShowCheap
+*/
+error_reporting('0');
+require 'fungsi.php';
+function sambung($db='mat_library', $host='localhost', $user='root', $pass=''){
+    @mysql_connect($host,$user,$pass) or die('<strong style="color: red;">Gagal Terhubung ke database '.mysql_error().'</strong>');
+    @mysql_select_db($db) or die('<strong style="color: red;">Gagal Memilih database</strong>');
+};
+
+function putus(){
+    mysql_close;
+}
+?>
